52a16811409c56c638a62b08daf072f9ef65efbe
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2024-09-21 18:40:09 +0100

    Implement sound effect and complete the preferences tab

--- src/components/PreferencesTab.tsx
+++ src/components/PreferencesTab.tsx
@@ -2,12 +2,16 @@
 import { MoonIcon, SunIcon, Volume2, VolumeX } from "lucide-react";
 import { Button } from "./ui/button";
 import { useTheme } from "next-themes";
-
+import { usePreferences } from "@/store/usePreferences";
+import { useSound } from "use-sound";
 
 const PreferencesTab = () => {
 	const { setTheme } = useTheme();
 
-	
+	const { soundEnabled, setSoundEnabled } = usePreferences();
+	const [playMouseClick] = useSound("/sounds/mouse-click.mp3");
+	const [playSoundOn] = useSound("/sounds/sound-on.mp3", { volume: 0.1 });
+	const [playSoundOff] = useSound("/sounds/sound-off.mp3", { volume: 0.1 });
 
 	return (
 		<div className='flex flex-wrap gap-2 px-1 md:px-2'>
@@ -16,7 +20,7 @@ const PreferencesTab = () => {
 				size={"icon"}
 				onClick={() => {
 					setTheme("light");
-					
+					soundEnabled && playMouseClick();
 				}}
 			>
 				<SunIcon className='size-[1.2rem] text-muted-foreground' />
@@ -26,7 +30,7 @@ const PreferencesTab = () => {
 				size={"icon"}
 				onClick={() => {
 					setTheme("dark");
-					
+					soundEnabled && playMouseClick();
 				}}
 			>
 				<MoonIcon className='size-[1.2rem] text-muted-foreground' />
@@ -34,9 +38,16 @@ const PreferencesTab = () => {
 			<Button
 				variant={"outline"}
 				size={"icon"}
-				
+				onClick={() => {
+					setSoundEnabled(!soundEnabled);
+					soundEnabled ? playSoundOff() : playSoundOn();
+				}}
 			>
-				<Volume2 className='size-[1.2rem] text-muted-foreground' />
+				{soundEnabled ? (
+					<Volume2 className='size-[1.2rem] text-muted-foreground' />
+				) : (
+					<VolumeX className='size-[1.2rem] text-muted-foreground' />
+				)}
 			</Button>
 		</div>
 	);
