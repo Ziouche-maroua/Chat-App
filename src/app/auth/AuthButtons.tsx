996c32cd0faee0673a3eb7d0786c6ce15454c060
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2024-09-23 21:30:03 +0100

    authentication setup sing Kinde

--- src/app/auth/AuthButtons.tsx
+++ src/app/auth/AuthButtons.tsx
@@ -1,16 +1,23 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useState } from "react";
 
 const AuthButtons = () => {
-	
+	const [isLoading, setIsLoading] = useState(false);
 
 	return (
 		<div className='flex gap-3 flex-1 md:flex-row flex-col relative z-50'>
-			<Button className='w-full' variant={"outline"} >
+			<RegisterLink className='flex-1' onClick={() => setIsLoading(true)}>
+				<Button className='w-full' variant={"outline"} disabled={isLoading}>
 					Sign up
 				</Button>
-                <Button className='w-full'>
-					Log in
+			</RegisterLink>
+			<LoginLink className='flex-1' onClick={() => setIsLoading(true)}>
+				<Button className='w-full' disabled={isLoading}>
+					Login
 				</Button>
+			</LoginLink>
 		</div>
 	);
 };
